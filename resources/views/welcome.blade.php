5b247826a0fdf593448c1be532a5dec5f71ea5c1
Shen PEI-RU <3a832056@gm.student.ncut.edu.tw>
2021-10-25 21:26:27 +0800

    練習 4 為 Route 命名

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -57,7 +57,11 @@
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
                                     Laravel has wonderful, thorough documentation covering every aspect of the framework. Whether you are new to the framework or have previous experience with Laravel, we recommend reading all of the documentation from beginning to end.
                                 </div>
+                                <a href="{{ route('hello.index') }}" class="underline">Hello</a>
+                                <a href = "{{route('hello.index', ['name'=>'tom'])}}" class="underline">Hello2</a>
+                                <a href = "/hello/tom" class="underline">Hello3</a>
                             </div>
+                          
                         </div>
 
                         <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('hello/{name?}',function($name='Everybody'){
     return'Hello,'.$name;
-});
+})->name('hello.index');
+
 Route::get('/',function(){
     return view('welcome');
 });
